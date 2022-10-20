360523d3830fc85430ad960d2800fbcab8cd144f
Paul <paulrousset@hotmail.fr>
2022-10-20 15:48:52 +0200

    add all mdels to admin panel

--- content_app/admin.py
+++ content_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from content_app.models import Content
+from content_app.models import Content, StreamPlatform, Review
 
 # Register your models here.
 admin.site.register(Content)
+admin.site.register(StreamPlatform)
+admin.site.register(Review)
--- content_app/api/urls.py
+++ content_app/api/urls.py
@@ -4,13 +4,13 @@ from rest_framework.routers import DefaultRouter
 from content_app.api import views
 
 router = DefaultRouter()
-router.register('stream/', views.StreamPlatformVS, basename='streamplatform')
+router.register('stream', views.StreamPlatformVS, basename='streamplatform')
 
 urlpatterns = [
     path('', include(router.urls)),
 
-    path('', views.ContentListAV.as_view(), name='movie-list'),
-    path('<int:pk>/', views.ContentDetailAV.as_view(), name='movie-detail'),
+    path('', views.ContentListAV.as_view(), name='content-list'),
+    path('<int:pk>/', views.ContentDetailAV.as_view(), name='content-detail'),
 
     path('<int:pk>/reviews/create/', views.ReviewCreate.as_view(), name='review-create'),
     path('<int:pk>/reviews/', views.ReviewList.as_view(), name='review-list'),
