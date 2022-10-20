7e602d63739eb6473245e3fe60aa5c5701213c63
Paul <paulrousset@hotmail.fr>
2022-10-20 14:07:37 +0200

    creating content model

--- content_app/admin.py
+++ content_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from content_app.models import Content
 
 # Register your models here.
+admin.site.register(Content)
--- content_app/models.py
+++ content_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Content(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+
+# The __str__ method in Python represents the class objects as a string
+    def __str__(self):
+        return self.name
