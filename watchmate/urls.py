6cc49d5f1428808fbd2481e754cb45beafe6be2c
Paul <paulrousset@hotmail.fr>
2022-10-21 17:38:35 +0200

    harmonize url patterns

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -4,5 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("dashboard/", admin.site.urls),
     path("api/watchmate/", include("content_app.api.urls")),
-    path("account/", include("user_app.api.urls")),
+    path("api/account/", include("user_app.api.urls")),
 ]
